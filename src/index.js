30a8f07c165d50cf7b1593bf55c7c15521ce136a
Janak Soni <Janak.X.Soni@kp.org>
2020-12-01 10:28:28 -0600

    Correctly Type-Checking Numbers

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ const app = document.getElementById('app');
 app.innerHTML = '<h1>JavaScript Basics</h1>';
 
 // ----
-const immutableNumber = 99;
-let referencedNumber = immutableNumber;
-referencedNumber = 44;
-console.log(immutableNumber);
-console.log(referencedNumber);
+console.log(typeof 99.66);
+console.log(99 instanceof Number);
+console.log(Number('99') instanceof Number);
+console.log(new Number('99') instanceof Number);
+console.log(Object.prototype.toString.call(99).slice(8, -1) === 'Number');
 
