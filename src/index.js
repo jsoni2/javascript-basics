816cac469dfc39305d2bd5620bcb7ff4cea5192e
Janak Soni <Janak.X.Soni@kp.org>
2020-12-01 16:36:39 -0600

    Strings and Immutability

--- src/index.js
+++ src/index.js
@@ -4,8 +4,7 @@ const app = document.getElementById('app');
 app.innerHTML = '<h1>JavaScript Basics</h1>';
 
 // ----
-console.log(new String(5599));
-console.log('Pepperoni'.length);
+const immutableString = 'I shall not change!';
+const uppercaseString = immutableString.toUpperCase();
 
-const pizza = 'P e p p e r o n i';
-console.log(pizza[0], pizza[pizza.length - 1], pizza.length);
+console.log(immutableString, uppercaseString);
