a422b69e9185abaf84a9949b0abd7691899655e0
Janak Soni <Janak.X.Soni@kp.org>
2020-12-01 10:49:13 -0600

    Exploring Number Methods

--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ const app = document.getElementById('app');
 app.innerHTML = '<h1>JavaScript Basics</h1>';
 
 // ----
-console.log(typeof 99.66);
-console.log(99 instanceof Number);
-console.log(Number('99') instanceof Number);
-console.log(new Number('99') instanceof Number);
-console.log(Object.prototype.toString.call(99).slice(8, -1) === 'Number');
-
+console.log(Number.prototype);
+console.log(parseFloat((99.12345678).toFixed(4)));
+console.log((99.12345678).toPrecision(5));
+console.log(new Number(99).valueOf());
