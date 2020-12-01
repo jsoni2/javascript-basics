f69f700b7f39309f75a5bc939839e06472964f45
Janak Soni <Janak.X.Soni@kp.org>
2020-12-01 10:12:48 -0600

    Numbers and Immutability

--- src/index.js
+++ src/index.js
@@ -4,16 +4,9 @@ const app = document.getElementById('app');
 app.innerHTML = '<h1>JavaScript Basics</h1>';
 
 // ----
-const result = Number('55px');
-
-// isNaN "NaN"
-console.log(isNaN(result));
-console.log(isNaN('I am a String!'));
-
-console.log(Number.NaN);
-console.log(Number.isNaN(result));
-console.log(Number.isNaN('I am another String!'));
-console.log(Number.isNaN(Number.NaN));
-
-console.log(Number.isInteger(66));
+const immutableNumber = 99;
+let referencedNumber = immutableNumber;
+referencedNumber = 44;
+console.log(immutableNumber);
+console.log(referencedNumber);
 
