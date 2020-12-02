dc1379a1f5ae1373bd5204298ea2df41fc9e8552
Janak Soni <Janak.X.Soni@kp.org>
2020-12-01 18:12:59 -0600

    Exploring String Methods

--- src/index.js
+++ src/index.js
@@ -4,8 +4,17 @@ const app = document.getElementById('app');
 app.innerHTML = '<h1>JavaScript Basics</h1>';
 
 // ----
-console.log(typeof 'Pepperoni');
-console.log('Pepperoni' instanceof String);
-console.log(new String('Pepperoni') instanceof String);
-console.log(String('Pepperoni') instanceof String);
-console.log(Object.prototype.toString.call('Pepperoni'));
+console.log(String.prototype);
+
+console.log('Pepperoni'.indexOf('P'));
+console.log('Pepperoni'.includes('P'));
+
+console.log('Pepperoni'.replace('oni', 'fire'));
+console.log('Pepperoni'.replace(/oni$/, 'fire'));
+
+console.log('Pepperoni'.slice(2, -3));
+
+console.log('Pepperoni~Plain'.split('~')[1]);
+console.log('Pepperoni~Plain'.split(/~/)[1]);
+
+console.log('  4444 5555 6666 7777  '.trim());
