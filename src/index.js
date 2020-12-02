41cdff2e40fabd97ac107c28da63660b3f729276
Janak Soni <Janak.X.Soni@kp.org>
2020-12-01 18:19:04 -0600

    Boolean Literal, Function and Constructor Syntax

--- src/index.js
+++ src/index.js
@@ -4,17 +4,12 @@ const app = document.getElementById('app');
 app.innerHTML = '<h1>JavaScript Basics</h1>';
 
 // ----
-console.log(String.prototype);
+console.log(true, false);
 
-console.log('Pepperoni'.indexOf('P'));
-console.log('Pepperoni'.includes('P'));
+const arg = 'X';
+console.log(Boolean(arg));
+console.log(!!arg);
+console.log(Boolean([]));
+console.log(Boolean({}));
 
-console.log('Pepperoni'.replace('oni', 'fire'));
-console.log('Pepperoni'.replace(/oni$/, 'fire'));
-
-console.log('Pepperoni'.slice(2, -3));
-
-console.log('Pepperoni~Plain'.split('~')[1]);
-console.log('Pepperoni~Plain'.split(/~/)[1]);
-
-console.log('  4444 5555 6666 7777  '.trim());
+console.log(new Boolean(arg));
